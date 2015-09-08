16d02ed9fa152b696e6b0266f78e23f94ca96fc0
arjun <sarodearjun57@gmail.com>
2015-09-07 22:47:50 -0500

    initial commit

---
+++ src/scripts/App.js
@@ -0,0 +1,22 @@
+'use strict';
+
+import React from 'react';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>Base</div>
+    );
+  }
+
+  static PropTypes = {}
+
+  static defaultProps = {}
+
+  static contextTypes = {}
+}
